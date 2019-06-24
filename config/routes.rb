f28663ea00b095c3e5a8b30a0654e8ada39370e1
Matt Winters <mwinters@colgate.edu>
2019-06-24 12:59:10 -0400

    created home page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :rental_properties
   root "rental_properties#index"
+
+  get '/home' => 'pages#home'
+  get '/about' => 'pages#about'
 end
