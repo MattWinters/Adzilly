f8df4d5febbc4b7b0effc53015610e211946049e
Matt Winters <mwinters@colgate.edu>
2019-06-25 17:46:14 -0400

    finished 'home page'

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :post_attachments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :rental_properties
-  root "rental_properties#index"
+  root "pages#home"
 
   get '/home' => 'pages#home'
   get '/about' => 'pages#about'
